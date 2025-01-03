8900997a04de639b09277c9f2c33476d9233840b
Zxl-Elena zhou <elena.xl.int@gmail.com>
2025-01-03 07:28:53 +0000

    feat: test function #1 #2

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -16,20 +16,20 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
     // #2
-    test('Test GET /hello with your name', function (done) {
+    test('Test GET /hello?name=<your_name>', function (done) {
       chai
         .request(server)
         .keepOpen()
         .get('/hello?name=xy_z')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
           done();
         });
     });
